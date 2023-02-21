a935ca9f1b1396113d332781818c43e00714ab1c
Ahmed TAHRI <ahmed@tripartie.com>
2023-02-21 09:38:15 +0100

    :sparkle: initial action refresh

--- tests/test-snap.sh
+++ tests/test-snap.sh
@@ -5,8 +5,6 @@ cd tests
 export PATH="/tmp/bin:$PATH"
 
 mkdir -p /tmp/bin
-cp ./helm-fake /tmp/bin/helm
-cp ./helm-fake /tmp/bin/helm3
 
 for s in $(find ./scenarios/ -mindepth 1 | grep -v 'snap'); do
   echo $s
